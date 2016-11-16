2da4a54577041dd7da6dd9cb9e8d2126fdfd8974
Jasse Lahdenperä <jasse.lahdenpera@rovio.com>
2016-11-16 22:30:44 +0200

    Added barebones file upload using multer.

--- Server/app/src/api/ocr.js
+++ Server/app/src/api/ocr.js
@@ -1,7 +1,34 @@
 import { Router } from 'express';
+import upload from '../index';
+import multer from 'multer';
+import mongoose from 'mongoose';
 
-export default ({ config }) => {
+export default ({ config }, upload) => {
 	let ocr = Router();
+
+	ocr.post('/upload', upload.array('photos[]', 15), function( req, res ) {
+		console.log(req);
+
+		req.files.forEach(function(file) {
+			var image = new Image({ 
+				name: file.originalname,
+				data: file.buffer
+			});
+
+			image.save();
+		});
+
+		res.json({
+			'text': 'Lorem ipsum dolor sit amet'
+		});
+	});
 
 	return ocr;
 }
+
+let Schema = mongoose.Schema;
+
+export const Image = mongoose.model('Image', new Schema({ 
+	name: String,
+	data: Buffer
+}));
