33662aa9228d38b366bd56f0fefd0d9a16594ae7
Bruzual Balzan Daniel <bruzuad1@niksula.hut.fi>
2016-12-02 16:58:41 +0200

    reorder code and add comments

--- Server/app/src/index.js
+++ Server/app/src/index.js
@@ -11,41 +11,45 @@ import history from './api/history';
 import { version } from '../package.json';
 import multer from 'multer';
 
+// Setup self-signed certificate
 const options = {
 	key: fs.readFileSync(__dirname + '/../ssl/key.pem'),
 	cert: fs.readFileSync(__dirname + '/../ssl/cert.pem')
 };
 
-let app = express();
-
-app.server = https.createServer(options, app);
-
-app.use(bodyParser.json({
-	limit : config.bodyLimit
-}));
-
+// setup middleware for handling multiform/form-data
 let memoryStorage = multer.memoryStorage();
 let upload = multer({
 	storage : memoryStorage
 });
 
+// setup mongodb connection
 mongoose.Promise = global.Promise;
 mongoose.set('debug', true);
-mongoose.connect(config.database); // connect to database
+mongoose.connect(config.database);
 
+
+let app = express();
+
+// setup middleware for accessing JSON request
+app.use(bodyParser.json({
+	limit : config.bodyLimit
+}));
+
+// API routes
 app.use('/users', users( { config } ) );
-
 app.use('/ocr', ocr( { config }, upload ) );
-
 app.use('/history', history( { config } ) );
 
-// expose some API metadata at the root
+// route for serving static files
+app.use(express.static(__dirname + '/public'));
+
+// simple endpoint to check server is running
 app.get('/', (req, res) => {
 	res.json({ version });
 });
 
-
-app.use(express.static(__dirname + '/public'));
+app.server = https.createServer(options, app);
 
 app.server.listen(process.env.PORT || config.port);
 
